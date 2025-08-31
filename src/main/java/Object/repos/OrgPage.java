70ecd702ff1bc02075deec48d84136e05e005790
sunil1097 <sunilsamal9770406@gmail.com>
2025-09-01 02:54:55 +0530

    updated pom and test script

--- src/main/java/Object/repos/OrgPage.java
+++ src/main/java/Object/repos/OrgPage.java
@@ -10,120 +10,84 @@ import com.crm.vtiger.generic_utility.WebDriverUtility;
 public class OrgPage {
 	WebDriver driver;
 	WebDriverUtility util;
+
 	public OrgPage(WebDriver driver) {
-		this.driver=driver;
+		this.driver = driver;
 		PageFactory.initElements(driver, this);
 	}
-	
-	// click on create + button  to create Org
-	@FindBy(xpath="//img[@title='Create Organization...']")
-	private WebElement createAOrg;
-	public WebElement getCreateOrg() {
-		return createAOrg;
-	}
+
+	// click on create + button to create Org
+	@FindBy(xpath = "//img[@title='Create Organization...']")
+	private WebElement btnCreateOrg;
+
 	// click on Org name input
-	@FindBy(name="accountname")
-	private WebElement orgName;
-	public WebElement getOrgName() {
-		return orgName;
-	}
+	@FindBy(name = "accountname")
+	private WebElement txtOrgNameInput;
+
 	// the next is save button
-	
-	@FindBy(xpath="//input[@type='button']")
-	private WebElement saveOrg;
-	public WebElement getSaveOrg() {
-		return saveOrg;
-	}
-	// edit the org
-	
+
+	@FindBy(xpath = "//input[@type='button']")
+	private WebElement btnSaveOrg;
+
+	// edit outside locators the org
+
 	// seach the org name on seach field
-	
-	@FindBy(name="search_text")
-	private WebElement searchIcon;
-	public WebElement getClickOnSearchIcon() {
-	return searchIcon;
-	}
-	
-	@FindBy(name="Edit")
-	private WebElement editOrgName;
-	public WebElement getEditOrgName() {
-		return editOrgName;
-	}
-	//then delete the  Org 
-	@FindBy(name="Delete")
+
+	@FindBy(name = "search_text")
+	private WebElement txtSearchOrg;
+
+	// select by org name
+	@FindBy(id = "bas_searchfield")
+	private WebElement ddOrgName;
+
+	// click on search now button
+
+	@FindBy(name = "submit")
+	private WebElement btnSearchNow;
+
+	@FindBy(linkText = "edit")
+	private WebElement lnkEditAction;
+
+	// then delete the Org
+	@FindBy(name = "Delete")
 	private WebElement deleteOrg;
+
 	public WebElement getDeleteTheOrg() {
 		return deleteOrg;
 	}
-	
-	// Edit Org
-	// search 
-	@FindBy(name ="search_text")
-	private WebElement searchTheOrgName;
-	public WebElement getTheOrgName() {
-		return searchTheOrgName;
+
+	// delete a product
+
+	@FindBy(linkText = "del")
+	private WebElement lnkDeleteAction;
+
+	// BUSINESS Action :1 Create Organization
+	public void createOrganization(String OrgName) {
+		btnCreateOrg.click();
+		txtOrgNameInput.sendKeys(OrgName);
+		btnSaveOrg.click();
 	}
-	//select by org name 
-	@FindBy(id="bas_searchfield")
-	private WebElement orgDropDown;
-	public WebElement getSearchOrgName() {
-		return orgDropDown;
-	}
-	// click on search now button 
-	
-	@FindBy(name="submit")
-	private WebElement clickSearchBtn;
-	public WebElement getClickOnSearchBtn() {
-		return clickSearchBtn;
-	}
-	// click on Org Row from Org details page 
-	@FindBy(linkText="edit")
-	private WebElement clickOnOrgDetailPage;
-	public WebElement  getClickOnOrgEditBtn() {
-		return clickOnOrgDetailPage;
-	}
-	
-	// delete the exiting org name 
-	@FindBy(linkText="del")
-	private WebElement delLink;
-	public WebElement getDelLink() {
-		return delLink;
-	}
-	
-	//BUSINESS UTILITY :1 Create Organization
-	public void createOrganization(String OrgNameText) {
-		createAOrg.click();
-		orgName.sendKeys(OrgNameText);
-		saveOrg.click();
-	}
-	//BUSINESS UTILITY :2 Edit existing Organization
-	public void editExistOrg(String accountname) {
-		getTheOrgName().clear();
-		getTheOrgName().sendKeys(accountname);
+
+	// BUSINESS Action :2 Edit existing Organization
+	public void editExistOrg(String createdOrgName, String updatedOrgName) {
+		txtSearchOrg.sendKeys(createdOrgName);
 		util = new WebDriverUtility(driver);
-		util.selectByVisibleText(orgDropDown, "Organization Name");
-		getClickOnSearchBtn().click();
-		getClickOnOrgEditBtn().click();
+		util.selectByVisibleText(ddOrgName, "Organization Name");
+		btnSearchNow.click();
+		lnkEditAction.click();
+		txtOrgNameInput.clear();
+		txtOrgNameInput.sendKeys(updatedOrgName);
 	}
-	
-	// inner edit 
-	public void editTheInternalOrgPage(String updatedOrgName) {
-		getOrgName().clear();
-		getOrgName().sendKeys(updatedOrgName);
-	}
-	//BUSINESS UTILITY :3 Delete the existing  Organization
-	public void deleteExistOrg(String accountname ) {
-		getTheOrgName().clear();
-		getTheOrgName().sendKeys(accountname);
+
+	// BUSINESS Action :3 Delete the existing Organization
+	public void deleteExistOrg(String OrgName) {
+		txtSearchOrg.sendKeys(OrgName);
 		util = new WebDriverUtility(driver);
-		util.selectByVisibleText(orgDropDown, "Organization Name");
-		getClickOnSearchBtn().click();
-		getDelLink().click();
+		util.selectByVisibleText(ddOrgName, "Organization Name");
+		btnSearchNow.click();
+		lnkDeleteAction.click();
 		util.acceptAlert();
 
 	}
-
-
-	
 
 }
--- src/test/java/com/crm/vtiger/Org/CreateAOrg.java
+++ src/test/java/com/crm/vtiger/Org/CreateAOrg.java
@@ -6,56 +6,58 @@ import org.testng.Assert;
 import org.testng.Reporter;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
+
 import com.crm.vtiger.generic_utility.BaseClass;
 import com.crm.vtiger.generic_utility.FileUtility;
 import com.crm.vtiger.generic_utility.JavaUtility;
 
-
 import Object.repos.OrgPage;
 import Object.repos.VerifyOrgPage;
 
-
-
 @Listeners(listner_utility.List_imp.class)
 public class CreateAOrg extends BaseClass {
+	String createdOrgName;
+	String OrgName;
 	OrgPage op;
 	String createdAOrgName;
 	VerifyOrgPage vOP;
-	@Test(priority =1)
+
+	@Test(priority = 1)
 	public void createProductTest() throws IOException, InterruptedException {
-		
-		// getting data from prop by utility file 
-		
+
+		// getting data from prop by utility file
+
 		FileUtility fUtil = new FileUtility();
-		// get data from excel file 
+		// get data from excel file
 		String Org = fUtil.getDataFromExcelFile2("Organization", 1, 0);
-		hp.navigateToOrg();	
+		hp.navigateToOrg();
 		op = new OrgPage(driver);
-		createdAOrgName = Org + JavaUtility.getRandomNumber();
-		op.createOrganization(createdAOrgName);
-		System.out.println("title after Org creation "+ driver.getTitle());
-		
+		createdOrgName = Org + JavaUtility.getRandomNumber();
+		op.createOrganization(createdOrgName);
+		System.out.println("title after Org creation " + driver.getTitle());
+
 		vOP = new VerifyOrgPage(driver);
 		String actualOrgName = vOP.getOrgNameVerify();
-		Assert.assertEquals(actualOrgName, createdAOrgName);
+		Assert.assertEquals(actualOrgName, createdOrgName);
 		Reporter.log("Successfully Org is created ");
-		
+
 	}
-	@Test(priority=2 ,dependsOnMethods ="createProductTest")
+
+	@Test(priority = 2, dependsOnMethods = "createProductTest")
 	public void editTheExistingOrg() {
 		hp.navigateToOrg();
 		op = new OrgPage(driver);
-		op.editExistOrg(createdAOrgName);
-		String updatedOrgName = "samal"+  JavaUtility.getRandomNumber();
-		op.editTheInternalOrgPage(updatedOrgName);
+		String updatedOrgName = "samal" + JavaUtility.getRandomNumber();
+		op.editExistOrg(createdOrgName, updatedOrgName);
 		Reporter.log("successfully edited ", true);
-		createdAOrgName=updatedOrgName;
+		createdOrgName = updatedOrgName;
 	}
-	@Test(priority=3, dependsOnMethods ="editTheExistingOrg")
+
+	@Test(priority = 3, dependsOnMethods = "editTheExistingOrg")
 	public void deleteTheExistitngOrg() {
 		hp.navigateToOrg();
 		op = new OrgPage(driver);
-		op.deleteExistOrg(createdAOrgName);
+		op.deleteExistOrg(createdOrgName);
 		Reporter.log("successfully deleted a product", true);
 	}
 
